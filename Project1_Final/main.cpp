5dc229d68592426ae721b2b17a92b4d74e91c92a
Calista Ruiz <167814650+calipalii@users.noreply.github.com>
2025-05-14 21:53:06 -0700

    main updated, code all transferred from net beans to vs code since git was being weirs with net beans

--- Project1_Final/main.cpp
+++ Project1_Final/main.cpp
@@ -1,9 +1,23 @@
 // Libraries
+#include "Game.h"
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    
+    Game game;
+    int numPlayers;
+    cout << "Enter number of players (2-4): ";
+    cin >> numPlayers;
+    if (numPlayers < 2 || numPlayers > 4) {
+        cout << "Invalid number of players.\n";
+        return 1;
+    }
+    for (int i = 0; i < numPlayers; ++i) {
+        string name;
+        cout << "Enter name for player " << i + 1 << ": ";
+        cin >> name;
+        game.addPlayer(name);
+    }
+    game.start();
     return 0;
 }
